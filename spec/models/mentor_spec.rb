00c6ad77fccd329984a0599beb2cff37027797d7
bryanelidimas <bryanelidimas@gmail.com>
2016-06-14 17:37:42 -0500

    Make test more descriptive - from be_truthy to exist expectation

--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe Mentor do
   it "creates a new mentor object" do
     mentor = Mentor.new
-    expect(mentor).to be_truthy
+    expect(mentor).to exist
   end
 end
