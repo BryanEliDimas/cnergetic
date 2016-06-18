51ede35294829c0be409cec8786051b1f102dc5a
bryanelidimas <bryanelidimas@gmail.com>
2016-06-18 17:25:34 -0500

    Validate presence of name

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  it { should validate_presence_of :name }
+
   it "can be created" do
     expect{ Task.create!(name: "Do my homework") }.to change(Task, :count).by(1)
   end
 
-  it "creates a new task" do
-    expect{ Task.create!(name: "Do my homework") }.to change(Task, :count).by(1)
+  it "requires a name at the dabase level" do
+    expect{ Task.new().save(validate: false) }.to raise_error(ActiveRecord::StatementInvalid)
   end
 
-  it "raises ActiveRecord error when created without a name" do
-    expect{ Task.create! }.to raise_error(ActiveRecord::StatementInvalid)
-  end
-
-  it { should validate_presence_of :name }
 end
