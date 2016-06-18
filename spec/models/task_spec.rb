a286806e0c9c135e79aa900f0e67911f4311c8bf
bryanelidimas <bryanelidimas@gmail.com>
2016-06-18 17:39:34 -0500

    Add a few more tests for Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   it { should validate_presence_of :name }
 
-  it "can be created" do
+  it "can be created with a name" do
     expect{ Task.create!(name: "Do my homework") }.to change(Task, :count).by(1)
   end
 
@@ -11,4 +11,17 @@ RSpec.describe Task, type: :model do
     expect{ Task.new().save(validate: false) }.to raise_error(ActiveRecord::StatementInvalid)
   end
 
+  it "can have a consequence" do
+    expect{ Task.create!(name: "Do my homework", consequence: 1) }
+  end
+
+  it "validates for consequence within range 1 to 10"
+
+  it "can have an estimated length"
+
+  it "validates estimated length is a positive value"
+
+  it "can have a due date"
+
+  it "validates due date is in the future"
 end
