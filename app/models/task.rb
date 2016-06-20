aa1aef8e14e843ddeb6e8b2278c4111e1c2b2319
bryanelidimas <bryanelidimas@gmail.com>
2016-06-20 15:44:09 -0500

    Make tests pass - add validations for Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   validates_presence_of :name
+  validates_numericality_of :consequence, allow_blank: true
+  validates_inclusion_of :consequence, in: 1..10, allow_blank: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it { should validate_presence_of :name }
+  it "requireds a name" do
+    should validate_presence_of :name
+  end
 
   it "can be created with a name" do
     expect{ Task.create!(name: "Do my homework") }.to change(Task, :count).by(1)
@@ -15,13 +17,18 @@ RSpec.describe Task, type: :model do
     expect{ Task.create!(name: "Do my homework", consequence: 1) }
   end
 
-  it "validates for consequence within range 1 to 10"
+  it "validates for consequence within range 1 to 10" do
+    should validate_numericality_of(:consequence)
+    should validate_inclusion_of(:consequence).in_range(1..10)
+  end
 
-  it "can have an estimated length"
+  it "can have an estimated length" do
+    expect{ Task.create!(name: "Do my homework", estimated_length: 15.minutes.from_now) }.to change(Task, :count).by(1)
+  end
 
-  it "validates estimated length is a positive value"
-
-  it "can have a due date"
+  it "can have a due date" do
+    expect{ Task.create!(name: "Do my homework", due: 15.minutes.from_now) }.to change(Task, :count).by(1)
+  end
 
   it "validates due date is in the future"
 end
